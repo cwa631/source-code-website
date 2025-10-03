856d604a8f53d352188198fecd18c9b72db099a8
ADITYA KUMAR SINGH <kumaraditya215117@gmail.com>
2025-10-03 15:51:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const projects = [
         source: 'https://buymeacoffee.com/codewithaditya631/e/451766',
         isFree: false
     },
-            {
+    {
         title: '✨ CSS Animated Responsive Cards Hover Effects',
         category: 'other',
         description: 'Enhance your website with modern, fully responsive animated cards built using only HTML & CSS. Lightweight, customizable, and mobile-friendly—perfect for portfolios, business sites, and product showcases.',
@@ -78,7 +78,17 @@ const projects = [
         demo: '#',
         source: 'https://buymeacoffee.com/codewithaditya631/e/456808',
         isFree: false
-    }
+    },
+{
+    title: 'CSS Glassmorphism Cards Hover Effects',
+    category: 'other',
+    description: 'Create stunning glassmorphism cards with hover effects using only HTML & CSS. Frosted glass blur, smooth hover animation, and modern UI design — perfect for websites & portfolios! 💎✨',
+    image: 'assets/Glassmorphism Cards Hover Effects .png',
+    demo: '#',
+    source: 'buymeacoffee.com/codewithaditya631/e/464321',
+    isFree: false
+}
+
 ];
 
 const grid = document.getElementById('projectsGrid');
